e6459acc5bb146bd0b2991b29a3fc2f535aff9e7
Luca Kim <seonjae.kic@khu.ac.kr>
2026-09-08 17:29:13 +0900

    style(scan): include professional scanner styles

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,6 +14,7 @@ import "./drop.css";
 import "./workstation.css";
 import "./multi-print.css";
 import "./scan.css";
+import "./scan-pro.css";
 
 export const metadata: Metadata = {
   title: "Print-cess by Club Paradiso",
